3522abec0d439820951dd700416e15f29d6c75e4
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-09-04 16:56:57 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,20 +11,19 @@ export class App extends Component {
     filter: '',
   };
 
-  componentDidMount(){
-    const savedContacts = localStorage.getItem("contacts");
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
     if (savedContacts !== null) {
       const contacts = JSON.parse(savedContacts);
-      this.setState({contacts})
+      this.setState({ contacts });
     }
   }
-  
-componentDidUpdate(prevProps, prevState){
-  if (this.state.contacts !== prevState.contacts) {
-    localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
-  }
-}
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value });
@@ -32,37 +31,41 @@ componentDidUpdate(prevProps, prevState){
 
   addContact = data => {
     const newContact = { ...data, id: nanoid() };
-    if (this.state.contacts.find(contact => contact.name.toLowerCase() === data.name.toLowerCase())) {
+    if (
+      this.state.contacts.find(
+        contact => contact.name.toLowerCase() === data.name.toLowerCase()
+      )
+    ) {
       alert(`${data.name} is already in contacts`);
-      return
-    } 
-      this.setState(prevState => {
-        return {
-          contacts: [...prevState.contacts, newContact],
-        };
-      });
-    
+      return;
+    }
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, newContact],
+      };
+    });
   };
 
-  handleDelete = (id) => {
+  handleDelete = id => {
     this.setState(prevState => {
-      return { contacts: prevState.contacts.filter(contact => contact.id !== id) }
-    })
-    
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
   };
 
   render() {
     const visibleItems = this.state.contacts.filter(contact =>
       contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
     );
-    
+
     return (
       <Container>
         <h1>Phonebook</h1>
         <ContactForm addContact={this.addContact} />
         <h2>Contacts</h2>
         <Filter handleChange={this.handleChange} />
-        <ContactList contacts={visibleItems} onDelete={this.handleDelete}/>
+        <ContactList contacts={visibleItems} onDelete={this.handleDelete} />
       </Container>
     );
   }
